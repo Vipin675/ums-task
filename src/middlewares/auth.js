e20aebcf6db447a731c9a3e8522e4da5dd004c3a
Vipin <vipinvishwa1m1@gmail.com>
2025-05-08 01:54:56 +0530

    JWT_SECRET usage updated in middlewares/auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 
+import { JWT_SECRET } from "../configs/index";
+
 const requireAuth = (req, res, next) => {
   const token = req.session?.jwt;
   if (!token) return res.redirect("/login");
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
